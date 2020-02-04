59fb0e9ebcc01d576c0dab41be55dc5877ae3668
Seth Goodrick <[goodrick001@gannon.edu]>
2020-02-04 16:03:19 -0500

    complete structure of dealer

--- src/blackjack_02/Dealer.java
+++ src/blackjack_02/Dealer.java
@@ -11,4 +11,24 @@ package blackjack_02;
  */
 public class Dealer {
     
+    private Player[] myPlayers;
+    
+    private Hand dealerHand = new Hand();
+        
+    private Deck myDeck = new Deck();
+ 
+    public Hand dealOpeningHand(){
+        return null; // change out of null!!!!!!!!!!!!!!!!!!
+    }
+    
+    public Hand playOutPlayerHands(){
+        return null; // change out of null!!!!!!!!!!!!! 
+    }
+    
+    public Hand playOutDealerHand(){
+        return null; //change out of null!!!!!!!!!!!!!!
+    }
+    public String declareWinner(){
+        return null; //change out of null!!!!!!!!!!! 
+    }
 }
