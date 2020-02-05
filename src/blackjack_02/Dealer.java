a5d9d00bd8d0c0c4cec72dba7b5181562934f54f
Seth Goodrick <[goodrick001@gannon.edu]>
2020-02-05 17:05:43 -0500

    complete dealer class structure and implemented initplayers method in dealer class

--- src/blackjack_02/Dealer.java
+++ src/blackjack_02/Dealer.java
@@ -5,30 +5,43 @@
  */
 package blackjack_02;
 
+import java.util.Scanner;
+
 /**
  *
  * @author gubotdev
  */
 public class Dealer {
+        
+    private Deck myDeck = new Deck();
     
     private Player[] myPlayers;
     
     private Hand dealerHand = new Hand();
-        
-    private Deck myDeck = new Deck();
- 
-    public void dealOpeningHand(){
-       
+    
+    Scanner scan = new Scanner(System.in);
+    
+    public Dealer (int numOfPlayers){
+         initPlayers(numOfPlayers);
     }
     
-    public void playOutPlayerHands(){
-       
+    public Dealer(){
+        System.out.println("How many want to play?");
+        int num = scan.nextInt();
+        initPlayers(num);
     }
     
-    public void playOutDealerHand(){
-        
-    }
-    public String declareWinner(){
-        return null; //change out of null!!!!!!!!!!! 
+    private void initPlayers(int numOfPlayers){
+        myPlayers = new Player[numOfPlayers];
+        for(int i = 0; i <myPlayers.length; i++){
+            System.out.println("Player " + i+1 + "whats your name");
+            String name = scan.next();
+            if(name.equals("")){
+                myPlayers [i] = new Player(i+1);
+            }else{
+              myPlayers[i] = new Player(name); 
+            }
+        }
     }
 }
+    
